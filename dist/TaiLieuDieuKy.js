9c6ea957f7b174ebb24606807b0927fea3eba7b4
thanhtunguet <thanhtung.uet@gmail.com>
2024-07-21 12:43:32 +0700

    remove dist

--- dist/TaiLieuDieuKy.js
+++
@@ -1,8 +0,0 @@
-var TaiLieuDieuKy = /** @class */ (function () {
-    function TaiLieuDieuKy() {
-    }
-    TaiLieuDieuKy.prototype.getListPages = function () { };
-    return TaiLieuDieuKy;
-}());
-export { TaiLieuDieuKy };
-export var tldk = new TaiLieuDieuKy();
